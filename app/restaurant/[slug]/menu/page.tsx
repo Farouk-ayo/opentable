00499678412b79b55c1475c1c603a4f4943c80b5
Farouk-ayo <mustaphafarouk41@gmail.com>
2023-12-25 22:44:42 +0100

    fetching data in server comp

--- app/restaurant/[slug]/menu/page.tsx
+++ app/restaurant/[slug]/menu/page.tsx
@@ -1,12 +1,34 @@
 import Header from "../components/Header";
 import RestaurantNavbar from "../components/RestaurantNavbar";
 import Menu from "../components/Menu";
+import { PrismaClient } from "@prisma/client";
 
-const RestaurantMenu = () => {
+const prisma = new PrismaClient();
+
+const fetchRestaurantMenu = async (slug: string) => {
+  const restaurant = await prisma.restaurant.findUnique({
+    where: {
+      slug,
+    },
+    select: {
+      items: true,
+    },
+  });
+  if (!restaurant) {
+    throw new Error("Restaurant not found");
+  }
+
+  return restaurant.items;
+};
+
+const RestaurantMenu = async ({ params }: { params: { slug: string } }) => {
+  const menu = await fetchRestaurantMenu(params.slug);
+  //   console.log(menu);
+
   return (
     <div className="bg-white w-[100%] rounded p-3 shadow">
-      <RestaurantNavbar />
-      <Menu />
+      <RestaurantNavbar slug={params.slug} />
+      <Menu menu={menu} />
     </div>
   );
 };
